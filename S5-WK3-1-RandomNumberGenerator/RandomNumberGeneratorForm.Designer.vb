da1b4d76c8ad189e27dc5e64b5dd68ec4e879dba
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-09-24 21:13:06 -0600

    Done

---
+++ S5-WK3-1-RandomNumberGenerator/RandomNumberGeneratorForm.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class RandomNumberGeneratorForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.GenerateButton = New System.Windows.Forms.Button()
+        Me.QuitButton = New System.Windows.Forms.Button()
+        Me.DisplayListBox = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'GenerateButton
+        '
+        Me.GenerateButton.Location = New System.Drawing.Point(12, 12)
+        Me.GenerateButton.Name = "GenerateButton"
+        Me.GenerateButton.Size = New System.Drawing.Size(170, 84)
+        Me.GenerateButton.TabIndex = 0
+        Me.GenerateButton.Text = "Generate"
+        Me.GenerateButton.UseVisualStyleBackColor = True
+        '
+        'QuitButton
+        '
+        Me.QuitButton.Location = New System.Drawing.Point(12, 102)
+        Me.QuitButton.Name = "QuitButton"
+        Me.QuitButton.Size = New System.Drawing.Size(170, 84)
+        Me.QuitButton.TabIndex = 1
+        Me.QuitButton.Text = "Quit"
+        Me.QuitButton.UseVisualStyleBackColor = True
+        '
+        'DisplayListBox
+        '
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 20
+        Me.DisplayListBox.Location = New System.Drawing.Point(188, 12)
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.Size = New System.Drawing.Size(107, 204)
+        Me.DisplayListBox.TabIndex = 2
+        '
+        'RandomNumberGeneratorForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(335, 230)
+        Me.Controls.Add(Me.DisplayListBox)
+        Me.Controls.Add(Me.QuitButton)
+        Me.Controls.Add(Me.GenerateButton)
+        Me.Name = "RandomNumberGeneratorForm"
+        Me.Text = "RandomNumberGeneratorForm"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GenerateButton As Button
+    Friend WithEvents QuitButton As Button
+    Friend WithEvents DisplayListBox As ListBox
+End Class
---
+++ S5-WK3-1-RandomNumberGenerator/RandomNumberGeneratorForm.vb
@@ -0,0 +1,35 @@
+﻿'Joshua Makuch
+'RCET 3371
+'Fall 2023
+'Random Number Generator
+'https://github.com/JoshuaMakuch/S5-WK3-1-RandomNumberGenerator
+
+Option Strict On
+Option Explicit On
+
+Public Class RandomNumberGeneratorForm
+
+    Dim random As New Random() 'Creates an instance of the random class and the new generates a new seed each run through to provide pseudo randomness
+
+    'Generate Button
+    Private Sub GenerateButton_Click(sender As Object, e As EventArgs) Handles GenerateButton.Click
+
+
+        'Clears the display list box for a new set of numbers
+        DisplayListBox.Items.Clear()
+
+        For n As Integer = 0 To 9
+
+            'The .next generates a random integer (>= first and < second), but .NextDouble can be used to generate a random double (0.000... to 0.999...)
+            DisplayListBox.Items.Add(n + 1 & ":       " & CStr(random.Next(-10, 11)))
+
+        Next
+
+    End Sub
+
+    'Quit Button
+    Private Sub QuitButton_Click(sender As Object, e As EventArgs) Handles QuitButton.Click
+        Me.Close()
+    End Sub
+
+End Class
